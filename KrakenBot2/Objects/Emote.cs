8247e4bac8e061b1fac197c4bdb5525ff218792d
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-01-28 19:20:32 -0700

    Actual object added.

---
+++ KrakenBot2/Objects/Emote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrakenBot2.Objects
+{
+    public class Emote
+    {
+        private string emote, channel;
+        private bool channelSpecific;
+
+        public string EmoteString { get { return emote; } }
+        public string Channel { get { return channel; } }
+        public bool ChannelSpecific { get { return channelSpecific; } }
+
+        public Emote(string emote, string channel, bool channelSpecific)
+        {
+            this.emote = emote;
+            this.channel = channel;
+            this.channelSpecific = channelSpecific;
+        }
+    }
+}
