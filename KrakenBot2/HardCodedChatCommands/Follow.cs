be4e2526a1555c737af666bbe9ef40e47a1264b0
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-01-28 18:46:09 -0700

    Corrects crash that resulted from TwitchLib not handling getTwitchChannel correctly.  Also, !follow attempts to search if it channel provided invalid or status nulled

--- KrakenBot2/HardCodedChatCommands/Follow.cs
+++ KrakenBot2/HardCodedChatCommands/Follow.cs
@@ -14,11 +14,31 @@ namespace KrakenBot2.HardCodedChatCommands
             {
                 if(Common.LastFollow == null || Common.LastFollow.Channel.Name.ToLower() != e.ArgumentsAsList[0].ToLower())
                     Common.LastFollow = WebCalls.getFollowData(e.ArgumentsAsList[0].ToLower()).Result;
-                Common.ChatClient.sendMessage(Common.LastFollow.ChatMessage, Common.DryRun);
+                if (Common.LastFollow != null)
+                    Common.ChatClient.sendMessage(Common.LastFollow.ChatMessage, Common.DryRun);
+                else
+                    Common.ChatClient.sendMessage(guessChannelName(e.ArgumentsAsList[0].ToLower()), Common.DryRun);
                 Common.command(e.Command, true);
             } else
             {
                 Common.command(e.Command, false);
+            }
+        }
+
+        private static string guessChannelName(string invalidChannel)
+        {
+            List<TwitchLib.TwitchChannel> results = TwitchLib.TwitchAPI.searchChannels(invalidChannel);
+            if (results.Count > 0)
+            {
+                TwitchLib.TwitchChannel bestGuess = results[0];
+                foreach (TwitchLib.TwitchChannel result in results)
+                    if (result.Followers > bestGuess.Followers)
+                        bestGuess = result;
+                return string.Format("The channel '{0}' is invalid or has no status.  Perhaps you meant '{1}'? In that case... {2}", invalidChannel, bestGuess.Name, WebCalls.getFollowData(bestGuess.Name).Result.ChatMessage);
+            }
+            else
+            {
+                return string.Format("The channel '{0}' is invalid or has no status, and I could not figure out which channel you meant :(", invalidChannel);
             }
         }
 
