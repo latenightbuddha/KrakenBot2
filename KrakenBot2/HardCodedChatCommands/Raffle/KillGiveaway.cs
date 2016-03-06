eb610c17fa0c68c99c19dcf228676f026bcfde13
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-03-05 20:05:41 -0700

    Added killgiveaway command static class

---
+++ KrakenBot2/HardCodedChatCommands/Raffle/KillGiveaway.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrakenBot2.HardCodedChatCommands.Raffle
+{
+    public static class KillGiveaway
+    {
+        public static void handleCommand(TwitchLib.TwitchChatClient.CommandReceivedArgs e)
+        {
+            if (verifyCommand(e) && !Common.DryRun)
+            {
+                if (Common.Raffle != null && Common.Raffle.raffleIsActive())
+                {
+                    Common.Raffle.killGiveaway();
+                    Common.ChatClient.sendMessage(string.Format("[KillGiveaway] Active giveaway for '{0}' has been killed!", Common.Raffle.RaffleName), Common.DryRun);
+                } 
+                else
+                {
+                    Common.ChatClient.sendMessage("[KillGiveaway] No active giveaway to kill!", Common.DryRun);
+                }
+
+                Common.command(e.Command, true);
+            }
+            else
+            {
+                Common.command(e.Command, false);
+            }
+        }
+
+        private static bool verifyCommand(TwitchLib.TwitchChatClient.CommandReceivedArgs e)
+        {
+            if (!Common.Cooldown.chatCommandAvailable(e.ChatMessage.UserType, e.Command, 0))
+                return false;
+            if (Common.DryRun)
+                return false;
+            if (!Common.isMod(e))
+                return false;
+            return true;
+        }
+    }
+}
