ca59b840aad07f8a41b64d959cede273d9847e9e
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-05-07 12:59:32 -0600

    Attempts to correct quote formatting logic a bit

--- KrakenBot2/Objects/Quote.cs
+++ KrakenBot2/Objects/Quote.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using System.Collections;
 
 namespace KrakenBot2.Objects
 {
@@ -17,6 +18,9 @@ namespace KrakenBot2.Objects
         public int ID { get { return id; } }
         public string QuoteContents { get { return quoteContents; } }
 
+        private static ArrayList authorSplits =
+            new ArrayList(new[] { " burkeblack", "burkeblack", "burkeblack ", "burkeblack 2014", "burke", " burke", " burke 2014", " burkeblack 2014", " burke 2015", "burke 2015", " burke 2015", " burkeblack 2015"});
+
         public Quote(JToken quoteProperties)
         {
             author = quoteProperties.SelectToken("author").ToString();
@@ -27,7 +31,7 @@ namespace KrakenBot2.Objects
                 string newCnts = "";
                 foreach(string section in quoteContents.Split('-'))
                 {
-                    if (section.ToLower() != " burkeblack" && section.ToLower() != " burke")
+                    if (!authorSplits.Contains(section.ToLower()))
                         newCnts = section;
                 }
                 quoteContents = newCnts;
